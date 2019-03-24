bb1e72f176327c0f212c8e83a4623ddbe053300a
Dwi Riyan Tono <dwiriyan855@gmail.com>
2019-03-25 04:51:26 +0700

    fix dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,35 +5,7 @@
     var base_url = "{{url('/')}}/";
 </script>
 <div class="row">
-    <div class="col-lg-3 col-xs-6" id="step1">
-        <!-- small box -->
-        <div class="small-box bg-aqua" id="total-news">
-            <div class="inner">
-                <h3 id="news-point">125</h3>
-
-                <p>Total Penjualan</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-newspaper-o"></i>
-            </div>
-            <a href="<?php echo route('/') ?>"
-               class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <div class="col-lg-3 col-xs-6" id="step1">
-        <!-- small box -->
-        <div class="small-box bg-green" id="total-page-view">
-            <div class="inner">
-            <h3 id="page-view-point">220</h3>
-                <p>Total Pembelian</p>
-            </div>
-            <div class="icon">
-                <i class="fa fa-bar-chart"></i>
-            </div>
-            <a href="<?php echo route('/') ?>"
-               class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+    
     <div class="col-lg-6 col-xs-12" id="step2">
         <div class="callout callout-info" style="min-height: 128px;">
             <h4>Shortcut Button!</h4>
@@ -41,13 +13,38 @@
             <p>Click button below for quick access</p>
 
             <p>
-            <a href="<?php echo route('/') ?>" class="btn bg-navy" ><i
-                    class="fa fa-edit"></i> Penjualan</a>
+            <a href="<?php echo route('penjualan-reguler') ?>" class="btn bg-navy" ><i
+                    class="fa fa-shopping-cart"></i> Penjualan</a>
         
-            <a href="<?php echo route('/') ?>" class="btn btn-warning" ><i class="fa fa-edit"></i>
+            <a href="<?php echo route('pembelian-reguler') ?>" class="btn btn-warning" ><i class="fa fa-shopping-cart"></i>
                 Pembelian</a>
+            <a href="<?php echo route('dashboard-po') ?>" class="btn btn-primary" ><i class="fa fa-shopping-cart"></i>
+                Pembelian PO</a>
                 
             </p>
+        </div>
+    </div>
+    <div class="col-lg-12 col-xs-12">
+
+        <div class="col-xs-12 col-md-12">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Stok Minimal</h3>
+                    <div class="box-tools pull-right">
+                    </div>
+                </div>
+                <div class="box-body no-padding">
+                    <div class="table-responsive">
+                        <?php echo $table; ?>
+                    </div>
+                </div>
+            </div>
+            <div class="clearfix" style="margin-bottom:20px;">
+                <div class="pull-left">
+                    Showing <strong><?php echo $offset + 1; ?></strong> - <strong><?php echo $offset+$limit > $total ? $total : $offset+$limit ;?></strong> of <strong><?php echo $total?></strong> data
+                </div>
+                <?php echo $pagination; ?>
+            </div>
         </div>
     </div>
 </div>
